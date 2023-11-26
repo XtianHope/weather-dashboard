35648334951f3ba35d6750a8ac9e730e424b4ba9
Christian Tino <xtianhope@gmail.com>
2023-11-26 18:09:29 -0500

    started building out city and weather information

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 // Geo Location API
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-// 5-Day Forecast API
+// Function to fetch data based on the longitude and latitudue
 function getGeoWeather(lat, lon) {
     fetch('http://api.openweathermap.org/data/2.5/forecast?appid=8823600ae11757d74ec67f06b60ca5ef&lat=' + lat + '&lon=' + lon + '&units=imperial')
     .then(function (response) {
@@ -16,6 +16,7 @@ function getGeoWeather(lat, lon) {
     })
 }
 
+// Function to fetch data based on the city name
 function getCityGeoData() {
     fetch('http://api.openweathermap.org/geo/1.0/direct?appid=8823600ae11757d74ec67f06b60ca5ef&limit=1&q=Charlotte')
     .then(function (response) {
