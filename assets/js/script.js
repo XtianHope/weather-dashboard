873c4931bbcc7cb1f6ed87fc951ca8ae9ea86a09
Christian Tino <xtianhope@gmail.com>
2023-11-26 17:47:09 -0500

    added functions to fetch data based on city name & longitude and latitude

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 // My OpenWeatherMap API key
-const apiKey = 8823600ae11757d74ec67f06b60ca5ef;
+// const apiKey = 8823600ae11757d74ec67f06b60ca5ef;
 
+// Geo Location API
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
+// 5-Day Forecast API
+function getGeoWeather(lat, lon) {
+    fetch('http://api.openweathermap.org/data/2.5/forecast?appid=8823600ae11757d74ec67f06b60ca5ef&lat=' + lat + '&lon=' + lon + '&units=imperial')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        getGeoWeather(data.lat, data.lon);
+    })
+}
+
+function getCityGeoData() {
+    fetch('http://api.openweathermap.org/geo/1.0/direct?appid=8823600ae11757d74ec67f06b60ca5ef&limit=1&q=Charlotte')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        getGeoWeather(data[0].lat, data[0].lon);
+    })
+}
+
+getCityGeoData();
 
