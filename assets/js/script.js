7c9a323b3a5286fafb86b4dd3f61f850feed0868
Christian Tino <xtianhope@gmail.com>
2023-11-26 19:54:03 -0500

    cleaned up JS and changed function to pull the necessary data- still not displaying 5 day and greensboro, durham, winston salem buttons are not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,44 @@
-// My OpenWeatherMap API key
-// const apiKey = 8823600ae11757d74ec67f06b60ca5ef;
-
-// Geo Location API
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-// Function to fetch data based on the longitude and latitudue
-function getGeoWeather(lat, lon) {
-    fetch('http://api.openweathermap.org/data/2.5/forecast?appid=8823600ae11757d74ec67f06b60ca5ef&lat=' + lat + '&lon=' + lon + '&units=imperial')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        getGeoWeather(data.lat, data.lon);
-    })
-}
-
-// Function to fetch data based on the city name
-function getCityGeoData() {
-    fetch('http://api.openweathermap.org/geo/1.0/direct?appid=8823600ae11757d74ec67f06b60ca5ef&limit=1&q=Charlotte')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        getGeoWeather(data[0].lat, data[0].lon);
-    })
-}
-
-getCityGeoData();
-
-// City Buttons
-const cityButtons = document.querySelectorAll('.city-button');
-
-// Show weather container for the selected/clicked city
-cityButtons.forEach(button => {
-    button.addEventListener('click', () => {
+function getWeather(city) {
+    const apiKey = '8823600ae11757d74ec67f06b60ca5ef';
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const tempElement = document.getElementById(`${city}-temp`);
+        const humidityElement = document.getElementById(`${city}-humidity`);
+        const windElement = document.getElementById(`${city}-wind`);
+  
+        tempElement.textContent = data.main.temp;
+        humidityElement.textContent = data.main.humidity;
+        windElement.textContent = data.wind.speed;
+      })
+      .catch(error => {
+        console.log('Error fetching weather data:', error);
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    // City Buttons
+    const cityButtons = document.querySelectorAll('.city-button');
+  
+    // Show weather container for the selected/clicked city
+    cityButtons.forEach(button => {
+      button.addEventListener('click', () => {
         const city = button.getAttribute('data-city');
         const cityContainer = document.getElementById(`${city}-container`);
-
+  
         // Hide all weather containers
         document.querySelectorAll('.weather-container').forEach(container => {
-            container.style.display = 'none';
+          container.style.display = 'none';
         });
-        
+  
         // Show weather container for selected/clicked city
         if (cityContainer) {
-            cityContainer.style.display = 'block';
+          cityContainer.style.display = 'block';
+          // Fetch weather data for the selected city
+          getWeather(city);
         }
+      });
     });
-});
+  });
